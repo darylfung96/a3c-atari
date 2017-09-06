fixed gradient apply to global network

main.py
@@ -1,15 +1,9 @@
 import threading
 import tensorflow as tf
 
-
-
-import signal
-import random
-import math
-import os
 import time
 
-THREAD_SIZE = 10
+THREAD_SIZE = 7
 SAVE_MODEL_RATE = 50    # Save model after this number of step
 
 from env import Env
@@ -25,8 +19,6 @@ allow_soft_placement=True))
 number_actions = create_env.number_actions()
 global_network = A3CLSTM(number_actions, -1)
 grad_applier = RMSApplier()
-#sess.run(tf.global_variables_initializer())
-
 
 threads = []
 

model.py
@@ -129,7 +129,7 @@ class A3CLSTM(object):
             #value loss will be 0.5 * advantage.
             #self.value_loss = tf.cumsum(0.5 * tf.square(self.td)) + 0.5 * self.value
             #################################
-            value_loss = 0.5 * tf.nn.l2_loss(self.discounted_rewards - self.value)
+            self.value_loss = 0.5 * tf.nn.l2_loss(self.discounted_rewards - self.value)
 
 # we need gaes, deltas, rewards, values, td, states
 
@@ -140,10 +140,10 @@ class A3CLSTM(object):
             log_policy = tf.log(self.policy + 1e-6)  # add a constant to prevent NaN
             entropies = -tf.multiply(log_policy, self.policy)
             # calculate policy loss
-            td = self.discounted_rewards - self.value
+            self.td = self.discounted_rewards - self.value
 
-            log_action = tf.multiply(log_policy, self.a) * td
-            self.policy_loss = -tf.reduce_sum(log_action - 0.01 * entropies)
+
+            self.policy_loss = -tf.reduce_sum(tf.multiply(log_policy, self.a) * self.td + 0.01 * entropies)
             #self.policy_loss = self.policy_loss - self.log_policy * self.gae - 0.01 * entropies
             """ end of for loops """
 

single_thread.py
@@ -33,8 +33,10 @@ class SingleThread:
         self.gradients = tf.gradients(self.local_network.total_loss, v_ref,
                                       colocate_gradients_with_ops=False, gate_gradients=False,
                                       aggregation_method=None)
-        self.apply_gradients = grad_applier.apply_gradient(self.global_network.get_vars(),
-                                                            self.gradients)
+       # self.apply_gradients = grad_applier.apply_gradient(self.global_network.get_vars(),
+       #                                                     self.gradients)
+
+        self.apply_gradients = tf.train.RMSPropOptimizer(initial_learning_rate).apply_gradients(zip(self.gradients, self.global_network.get_vars()))
 
         self.sync = self.local_network.sync_from(self.global_network)
 
@@ -79,12 +81,10 @@ class SingleThread:
         if self.done:
             self.state = self.env.reset()
             self.done = False
-            #self.write_summary(sess, train_writer, score, summary_op, global_step)
-           # print(self.episode_reward)
-            #self.episode_reward = 0
 
         # now our local network is the same as global network
         for i in range(0, LOCAL_MAX_STEP):
+            #self.env.render()
             policy, value = self.local_network.get_policy_value(sess, self.state)
             action = self.choose_action(policy)
 
@@ -115,15 +115,13 @@ class SingleThread:
             _, R = self.local_network.get_policy_value(sess, self.state) # run and get the last value
             R = R[0]
             #states.append(self.state)
-            #R = value[0]
 
-        #values.append(R)
 
         a = []
         action_batch = []
         for i in reversed(range(len(rewards))):
             R = R * gamma + rewards[i]
-            R = R - values[i]
+            #R = R - values[i] # this is temporal difference
             discounted_rewards.append(R)
             a = np.zeros(self.action_size)
             a[actions[i]] = 1
@@ -140,16 +138,15 @@ class SingleThread:
         states.reverse()
         states = np.array(states).reshape(-1, 47, 47, 1)
         discounted_rewards = np.array(discounted_rewards).reshape(-1, 1)
-        rewards.reverse()
-        values.reverse()
+        #rewards.reverse()
 
 
         _, summary = sess.run([self.apply_gradients, summary_op],
                      feed_dict={
                          self.local_network.s: states,
-                         self.local_network.rewards: rewards,
+                         #self.local_network.rewards: rewards,
                          #self.local_network.values: values,
-                         self.local_network.step_size: [len(actions)],
+                         self.local_network.step_size: [len(states)],
                          #self.local_network.deltas: deltas,
                         # self.local_network.gaes: gaes,
                          #self.local_network.td: td,
@@ -162,4 +159,4 @@ class SingleThread:
         self.write_summary(summary, train_writer, global_step)
 
 
-
+        time.sleep(2)